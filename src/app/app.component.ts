557b3d6802d679fa5e8a87be2117b47988f17952
Guilherme Moura <guileon64@gmail.com>
2021-04-29 14:25:00 -0300

    versao 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,9 @@ export class AppComponent {
   public lista1 = [['Maria', '28', 'Belo Horizonte', 'Pará de Minas'], ['Claudio', '26', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro', 'teste']]
   public campos1 = ['Nome', 'Idade', 'Cidade Atual', 'Cidade do Nascimento'];
 
-  public lista2 = [['Maria', '28', 'Belo Horizonte', 'Pará de Minas'], ['Claudio', '26', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro', 'teste'],
-  ['Maria', '28', 'Belo Horizonte', 'Pará de Minas'], ['Claudio', '26', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro', 'teste'],
-  ['Maria', '28', 'Belo Horizonte', 'Pará de Minas'], ['Claudio', '26', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro', 'teste']];
+  public lista2 = [['Maria', '28', 'Belo Horizonte', 'Pará de Minas'], ['Claudio', '26', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro'],
+  ['Luzia', '28', 'Belo Horizonte', 'Pará de Minas'], ['Otavio', '32', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro'],
+  ['Sinead', '28', 'Belo Horizonte', 'Pará de Minas'], ['Matheus', '35', 'São Paulo', 'São Paulo'], ['Alice', '34', 'Niterói', 'Rio de Janeiro']];
 
 
   public legenda = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -10,13 +10,52 @@ export class TabelaComponent implements OnInit {
   @Input() titulo : String;
   @Input() subtitulo : String;
   @Input() legenda : String;
-  @Input() tamanho = 5;
+  @Input() tamanho : number;
   @Input() colunas = [];
   @Input() dados = [];
+  
 
-  constructor() { }
+  public dadosAtuais : Array<Array<String>>;
+  public numPaginas : number = 1;
+  public paginaAtual: number = 1;
+
+
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
+    
+    this.paginaAtual = 1;
+    this.numPaginas = Math.ceil(this.dados.length/this.tamanho);
+    this.dadosAtuais = new Array(this.numPaginas);
+    var emptyData : Array<String> = new Array(this.colunas.length).fill("-");
+    var k  = 0;
+    for(var i = 0; i < this.numPaginas; i++){
+      this.dadosAtuais[i] = new Array(this.tamanho);
+      for(var j = 0; j < this.tamanho; j++){
+        if(k < this.dados.length){
+          this.dadosAtuais[i][j] = this.dados[k];
+          k++;
+        }else{
+          this.dadosAtuais[i][j] = emptyData as unknown as any;
+        }
+      }
+    }
+  }
+
+  mudarPagina(valor){
+    if((valor + this.paginaAtual) < 1) this.paginaAtual = this.numPaginas;
+    else if ((valor + this.paginaAtual) > this.numPaginas) this.paginaAtual = 1;
+    else this.paginaAtual +=valor;
+  }
+
+  primeiro(){
+    this.paginaAtual = 1;
+  }
+
+  ultimo(){
+    this.paginaAtual = this.numPaginas;
   }
 
 }
