167becc777c7cec5c11ab12c2236ee63110d2b99
Guilherme Moura <guileon64@gmail.com>
2021-05-03 09:45:31 -0300

    correcao de amostragem de numeros de paginacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,9 @@ export class AppComponent {
 
   public lista2 = [['Maria', '28', 'Belo Horizonte', 'Pará de Minas'], ['Claudio', '26', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro'],
   ['Luzia', '28', 'Belo Horizonte', 'Pará de Minas'], ['Otavio', '32', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro'],
+  ['Ana', '28', 'Belo Horizonte', 'Ipatinga'], ['Otavio', '32', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro'],
+  ['Eva', '27', 'Betim', 'Azurita'], ['Otavio', '32', 'São Paulo', 'Jundiaí'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro'],
+  ['Otavio', '25', 'Contagem', 'Florestal'], ['Otavio', '32', 'São Paulo', 'São Paulo'], ['Sandro', '34', 'Niterói', 'Rio de Janeiro'],
   ['Sinead', '28', 'Belo Horizonte', 'Pará de Minas'], ['Matheus', '35', 'São Paulo', 'São Paulo'], ['Alice', '34', 'Niterói', 'Rio de Janeiro']];
 
 
--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -7,55 +7,73 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TabelaComponent implements OnInit {
 
-  @Input() titulo : String;
-  @Input() subtitulo : String;
-  @Input() legenda : String;
-  @Input() tamanho : number;
+  @Input() titulo: String;
+  @Input() subtitulo: String;
+  @Input() legenda: String;
+  @Input() tamanho: number;
   @Input() colunas = [];
   @Input() dados = [];
-  
 
-  public dadosAtuais : Array<Array<String>>;
-  public numPaginas : number = 1;
+
+  public dadosAtuais: Array<Array<String>>;
+  public numPaginas: number = 1;
   public paginaAtual: number = 1;
+  public menuInferior: number[];
 
 
   constructor() {
-    
+
   }
 
   ngOnInit(): void {
-    
+
     this.paginaAtual = 1;
-    this.numPaginas = Math.ceil(this.dados.length/this.tamanho);
+    this.numPaginas = Math.ceil(this.dados.length / this.tamanho);
     this.dadosAtuais = new Array(this.numPaginas);
-    var emptyData : Array<String> = new Array(this.colunas.length).fill("-");
-    var k  = 0;
-    for(var i = 0; i < this.numPaginas; i++){
+    var emptyData: Array<String> = new Array(this.colunas.length).fill("-");
+    var k = 0;
+    for (var i = 0; i < this.numPaginas; i++) {
       this.dadosAtuais[i] = new Array(this.tamanho);
-      for(var j = 0; j < this.tamanho; j++){
-        if(k < this.dados.length){
+      for (var j = 0; j < this.tamanho; j++) {
+        if (k < this.dados.length) {
           this.dadosAtuais[i][j] = this.dados[k];
           k++;
-        }else{
+        } else {
           this.dadosAtuais[i][j] = emptyData as unknown as any;
         }
       }
     }
+    this.gerarVetor();
   }
 
-  mudarPagina(valor){
-    if((valor + this.paginaAtual) < 1) this.paginaAtual = this.numPaginas;
+  mudarPagina(valor) {
+    if ((valor + this.paginaAtual) < 1) this.paginaAtual = this.numPaginas;
     else if ((valor + this.paginaAtual) > this.numPaginas) this.paginaAtual = 1;
-    else this.paginaAtual +=valor;
+    else this.paginaAtual += valor;
+    this.gerarVetor();
   }
 
-  primeiro(){
+  primeiro() {
     this.paginaAtual = 1;
+    this.gerarVetor();
   }
 
-  ultimo(){
+  ultimo() {
     this.paginaAtual = this.numPaginas;
+    this.gerarVetor();
   }
 
+  goToPagina(value) {
+    this.paginaAtual = value;
+    this.gerarVetor();
+  }
+
+  gerarVetor() {
+    this.menuInferior = [];
+    var itemMax = 3;
+    if(this.numPaginas%2 == 0) itemMax=4;
+    for (var i = -2; i < itemMax; i++) {
+      this.menuInferior.push(this.paginaAtual + i);
+    }
+  }
 }
